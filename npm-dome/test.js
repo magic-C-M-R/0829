64fc2322aed5539279fa3fe966cc2255ec697d90
magic-C-M-R <1941600049@qq.com>
2017-08-30 15:56:05 +0800

    mmmm

---
+++ npm-dome/test.js
@@ -0,0 +1,11 @@
+// var $ =require('./node_modules/jquery')
+// // require 运用于接口
+// console.log($)
+var fun = 10;
+var a =require('./hellow.js')
+console.log(a(1,2));
+console.log(fun);
+
+
+//dependencises 
+//dovdependencises //工具
